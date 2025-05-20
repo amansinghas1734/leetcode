325a4ec63220a7ba2bf36abc1e3605666bd85fd4
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-05-20 12:28:33 +0530

    Time: 151 ms (12.02%), Space: 325.3 MB (33.95%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<int> v(n,0);
+        for(auto x:queries){
+            v[x[0]]++;
+            if(x[1]+1<n){
+                v[x[1]+1]--;
+            }
+        }
+        for(int i=1;i<n;i++){
+            v[i]+=v[i-1];
+        }
+        for(int i=0;i<n;i++){
+            if(v[i]<nums[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
