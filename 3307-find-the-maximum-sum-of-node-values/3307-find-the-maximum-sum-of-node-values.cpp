9393fb29b27871e27b98858338dd33369ae37492
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-05-23 22:35:24 +0530

    Time: 0 ms (100%), Space: 126.5 MB (98.06%) - LeetHub

--- 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.cpp
@@ -16,26 +16,4 @@ public:
         }
         return c%2==0?s:s-nuk;
     }
-    // long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-    //     long long  sum        = 0;
-    //     int count      = 0;
-    //     int minNukasan = INT_MAX;
-
-    //     for (long long num : nums) {
-
-    //         if((num^k) > num) {
-    //             count++;
-    //             sum += (num^k);
-    //         } else {
-    //             sum += num;
-    //         }
-
-    //         minNukasan = min((long long)minNukasan, abs(num - (num ^ k)));
-    //     }
-
-    //     if(count%2 == 0)
-    //         return sum;
-
-    //     return sum - minNukasan;
-    // }
 };
