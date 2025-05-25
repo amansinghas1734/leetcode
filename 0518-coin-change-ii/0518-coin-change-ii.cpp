cc2bb5af10ba4661435ca9665b23799b944dd528
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-05-25 22:49:37 +0530

    Time: 23 ms (45.77%), Space: 16.3 MB (73.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int  v[5002][302];
-    int solve(int a,vector<int>& c,int i){
+int t[5001][301];
+    int solve(int a, vector<int>& c,int i){
         if(a==0){
             return 1;
         }
         if(a<0||i>=c.size()){
             return 0;
         }
-        if(v[a][i]!=-1){
-            return v[a][i];
+        if(t[a][i]!=-1){
+            return t[a][i];
         }
-        return v[a][i]=solve(a-c[i],c,i)+solve(a,c,i+1);
+        return t[a][i]=solve(a-c[i],c,i)+solve(a,c,i+1);
     }
-    int change(int amount,vector<int>& coins) {
-        memset(v,-1,sizeof(v));
+    int change(int amount, vector<int>& coins) {
+        memset(t,-1,sizeof(t));
         return solve(amount,coins,0);
     }
 };
