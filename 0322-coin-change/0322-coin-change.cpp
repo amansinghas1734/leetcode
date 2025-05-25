55bcf5e19941d840fe4e483d9114b924e7e7966f
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-05-25 22:14:00 +0530

    Time: 32 ms (54.56%), Space: 13.7 MB (95.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,14 +4,12 @@ public:
 
     int solve(vector<int>& c, int a, int j) {
         if (a == 0) return 0;
-        if (a < 0 || j >= c.size()) return INT_MAX;
+        if (a < 0 || j >= c.size()) return INT_MAX-1;
 
         if (v[a][j] != -1) return v[a][j];
 
-        int take = solve(c, a - c[j], j);
+        int take = 1+solve(c, a - c[j], j);
         int skip = solve(c, a, j + 1);
-
-        if (take != INT_MAX) take += 1; 
 
         return v[a][j] = min(take, skip);
     }
@@ -19,6 +17,6 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         memset(v, -1, sizeof(v));
         int x = solve(coins, amount, 0);
-        return x == INT_MAX ? -1 : x;
+        return x == INT_MAX-1? -1 : x;
     }
 };
