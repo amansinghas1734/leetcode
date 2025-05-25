1dfc477e4e4cb74e1859bab6b72fc736d36d89dd
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-05-25 22:41:26 +0530

    Time: 30 ms (57.62%), Space: 13.7 MB (95.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int v[10002][14]; 
-
-    int solve(vector<int>& c, int a, int j) {
-        if (a == 0) return 0;
-        if (a < 0 || j >= c.size()) return INT_MAX-1;
-
-        if (v[a][j] != -1) return v[a][j];
-
-        int take = 1+solve(c, a - c[j], j);
-        int skip = solve(c, a, j + 1);
-
-        return v[a][j] = min(take, skip);
+    int t[10001][13];
+    int solve(vector<int>& c, int a,int i){
+        if(a==0){
+            return 0;
+        }
+        if(a<0||i>=c.size()){
+            return INT_MAX-1;
+        }
+        if(t[a][i]!=-1){
+            return t[a][i];
+        }
+        return t[a][i]=min(1+solve(c,a-c[i],i),solve(c,a,i+1));
     }
-
     int coinChange(vector<int>& coins, int amount) {
-        memset(v, -1, sizeof(v));
-        int x = solve(coins, amount, 0);
-        return x >= INT_MAX-1? -1 : x;
+        memset(t,-1,sizeof(t));
+        int x=solve(coins,amount,0);
+        return x>=INT_MAX-1?-1:x;
     }
 };
