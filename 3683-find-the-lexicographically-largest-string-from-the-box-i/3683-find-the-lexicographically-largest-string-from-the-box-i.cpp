e23119eca4084731693e3833b7078a621c98c355
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-06-05 00:13:46 +0530

    Time: 25 ms (73.51%), Space: 10.2 MB (93.66%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    string ans;
-    void solve(string s){
-        int i=0,j=0;
-        while(i<ans.size()&&j<s.size()){
-            if(ans[i]==s[j]){
-                i++;
-                j++;
-            }
-            else if(ans[i]<s[j]){
-                ans=s;
-                return;
-            }
-            else{
-                return;
-            }
-        }
-        if(i==ans.size()){
-            ans=s;
-        }
-    }
     string answerString(string word, int numFriends) {
+        int l=word.length();
         if(numFriends==1){
             return word;
         }
-        int l=word.length();
-        int a=l-numFriends;
-        a++;
-        ans=word.substr(0,a);
-        for(int i=1;i<l-a;i++){
-            solve(word.substr(i,a));
+        int i=0;
+        int j=0;
+        while(j<word.size()){
+            if(word[j]>word[i]){
+                i=j;
+                j++;
+                continue;
+            }
+            if(word[j]==word[i]){
+                int k=1;
+                while(j+k<l&&k<l-(numFriends-1)){
+                    if(word[j+k]>word[i+k]){
+                        i=j;
+                        j=j+k-1;
+                        break;
+                    }
+                    else if(word[j+k]<word[i+k]){
+                        j=j+k-1;
+                        break;
+                    }
+                    k++;
+                }
+            }
+            j++;
         }
-        int i=l-a;
-        while(i<l){
-            solve(word.substr(i));
-            i++;
-        }
-        return ans;
+        return word.substr(i,min(l-numFriends+1,l-i));
     }
 };
