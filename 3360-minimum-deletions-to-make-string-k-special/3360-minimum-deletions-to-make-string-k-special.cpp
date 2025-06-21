23c98428b0100445f345c5c021673e936f36d4a4
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-06-21 14:02:43 +0530

    Time: 1 ms (89.13%), Space: 19.6 MB (41.3%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,48 @@
+class Solution 
+{
+public:
+    int minimumDeletions(string word, int k) 
+    {
+        vector<int> count(26, 0);
+        for (char c : word) 
+        {
+            count[c - 'a']++;
+        }
+
+        vector<int> freqs;
+        for (int c : count) 
+        {
+            if (c > 0)
+            {
+                freqs.push_back(c);
+            } 
+        }
+        sort(freqs.begin(), freqs.end());
+        int n = freqs.size();
+
+        vector<int> prefix(n + 1, 0);
+        for (int i = 0; i < n; ++i) 
+        {
+            prefix[i + 1] = prefix[i] + freqs[i];
+        }
+
+        int total = prefix[n];
+        int minDeletions = INT_MAX;
+
+        for (int i = 0; i < n; ++i) 
+        {
+            int target = freqs[i];
+            int maxAllowed = target + k;
+
+            int j = upper_bound(freqs.begin(), freqs.end(), maxAllowed) - freqs.begin();
+
+            int deleteBelow = prefix[i];
+            int deleteAbove = total - prefix[j] - (maxAllowed * (n - j));
+            int deletions = deleteBelow + deleteAbove;
+
+            minDeletions = min(minDeletions, deletions);
+        }
+
+        return minDeletions;
+    }
+};
