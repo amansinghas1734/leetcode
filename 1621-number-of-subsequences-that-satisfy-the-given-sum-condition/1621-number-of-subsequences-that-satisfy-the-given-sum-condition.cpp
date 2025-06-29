25f58e3b91dc86777fb406438cae0c945cb099fd
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-06-29 17:56:12 +0530

    Time: 29 ms (71.12%), Space: 54.3 MB (34.63%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -11,7 +11,7 @@ public:
         int i=0,j=nums.size()-1;
         while(i<=j){
             if(nums[i]+nums[j]<=target){
-                ans=((ans%mod)+pow[j-i])%mod;
+                ans=(ans+pow[j-i])%mod;
                 i++;
             }
             else(j--);
