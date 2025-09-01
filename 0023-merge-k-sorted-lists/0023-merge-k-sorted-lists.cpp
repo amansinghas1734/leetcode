43947ec2be61beb029c95d81475a0ee1a6f75fc8
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-09-01 21:10:53 +0530

    Time: 1 ms (76.06%), Space: 18.5 MB (50.8%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,12 +8,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Helper to compare ListNode pointers in the priority_queue
 struct Compare {
     bool operator()(ListNode* a, ListNode* b) const {
-        // min-heap: smaller value has higher priority
         if (a->val != b->val) return a->val > b->val;
-        // If values are equal, use the pointer address to ensure a strict weak ordering
         return a > b;
     }
 };
@@ -21,10 +18,8 @@ struct Compare {
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // Priority queue (min-heap) holding ListNode pointers
         priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
 
-        // Initialize heap with the head of each non-empty list
         for (ListNode* node : lists) {
             if (node) pq.push(node);
         }
