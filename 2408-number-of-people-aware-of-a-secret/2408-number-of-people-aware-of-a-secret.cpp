584e31101cc16d255e3b33a324d501d00368bd5c
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-09-09 20:31:26 +0530

    Time: 2 ms (63.37%), Space: 9.5 MB (39.85%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int MOD=1e9+7;
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long long> dp(n + 1, 0);
+        dp[1] = 1;
+        long long share = 0;
+        for (int t = 2; t <= n; t++) {
+            if (t - delay > 0)
+                share = (share + dp[t - delay] + MOD) % MOD;
+            if (t - forget > 0)
+                share = (share - dp[t - forget] + MOD) % MOD;
+            dp[t] = share;
+        }
+        long long know = 0;
+        for (int i = n - forget + 1; i <= n; i++)
+            know = (know + dp[i]) % MOD;
+        return (int)know;
+    }
+};
