75c7b1d3a75a2b23e054b6f44256821afec79c06
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2025-09-28 12:53:33 +0530

    Time: 17 ms (10.54%), Space: 25.5 MB (95.69%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=nums.size()-2;i>0;i--){
             int a=nums[i-1],b=nums[i],c=nums[i+1];
-            if(a+b>c&&b+c>a&&a+c>b){
+            if(a+b>c){
                 ans=a+b+c;
                 break;
             }
