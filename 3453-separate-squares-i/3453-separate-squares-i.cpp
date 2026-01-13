2a4045e2c6f924047fc84a85b90754a3ec6b5e24
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2026-01-14 00:49:26 +0530

    Time: 80 ms (84.68%), Space: 198.7 MB (52.02%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    static double area_geq_y(vector<vector<int>>& squares, const double y) {
+        double A=0;
+        for (auto& s : squares) {
+            double L=s[2], y0=s[1]+L; 
+            if (y>=y0) continue;  
+            double h=min(L, y0-y);  
+            A+=L*h;
+        }
+        return A;
+    }
+
+    static double separateSquares(vector<vector<int>>& squares) {
+        double A0=area_geq_y(squares, -1e9); 
+        double A2=A0/2.0;  
+        double minY=INT_MAX, maxY=INT_MIN;
+        for (auto& s : squares) {
+            double L=s[2], y0=s[1];
+            minY=min(minY, y0);
+            maxY=max(maxY, y0+L);
+        }
+
+        double l=minY, r=maxY, m;
+        const double epsilon=1e-5;  
+
+        while (r-l> epsilon) {
+            m=(l+r)/ 2;
+            double A=area_geq_y(squares, m);
+
+            if (A>A2) l=m;  
+            else r=m;  
+        }
+        return l;
+    }
+};
