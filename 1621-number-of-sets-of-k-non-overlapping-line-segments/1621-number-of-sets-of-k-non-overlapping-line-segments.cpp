13eb5573e0641f285e22ffef0109071eae9c5d48
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2026-09-17 00:39:40 +0530

    Time: 67 ms (48.6%), Space: 16.7 MB (61.21%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -2,25 +2,30 @@
 class Solution {
 public:
     int MOD = 1e9 + 7;
+    int dp[1001][1001][2];
+
+    int solve(int i, int k, int open, int n) {
+        if (k < 0) return 0;
+        if (i == n) {
+            return (k == 0 && open == 0);
+        }
+        if (dp[i][k][open] != -1)
+            return dp[i][k][open];
+        long long ans = 0;
+        if (open == 0) {
+            ans += solve(i + 1, k, 0, n);
+            ans += solve(i + 1, k, 1, n);
+        }
+        else {
+            ans += solve(i + 1, k, 1, n);
+            ans += solve(i + 1, k - 1, 0, n);
+            ans += solve(i + 1, k - 1, 1, n);
+        }
+        return dp[i][k][open] = ans % MOD;
+    }
 
     int numberOfSets(int n, int k) {
-        vector<vector<long long>> dp(n + k, vector<long long>(2 * k + 1, 0));
-
-        for (int i = 0; i < n + k; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = 1; i < n + k; i++) {
-            for (int j = 1; j <= 2 * k; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j <= i) {
-                    dp[i][j] += dp[i - 1][j - 1];
-                    dp[i][j] %= MOD;
-                }
-            }
-        }
-
-        return dp[n + k - 1][2 * k];
+        memset(dp, -1, sizeof(dp));
+        return solve(0, k, 0, n);
     }
 };
