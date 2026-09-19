65059a2856fb0893141fd4e67b1899e89681c717
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2026-09-20 00:53:04 +0530

    Time: 12 ms (44.11%), Space: 21.9 MB (41.73%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n=start.length();
+        for(int i=0, j=0; i<n||j<n;i++, j++){
+            while(start[i]=='_') i++;
+            while(target[j]=='_') j++;
+            char c=start[i];
+            if(c!=target[j]) return false;
+            if(c=='L'&&i<j) return false;
+            if(c=='R'&&i>j) return false;
+        }
+        return true;
+    }
+};
