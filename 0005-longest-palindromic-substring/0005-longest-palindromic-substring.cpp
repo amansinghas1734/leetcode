0712ae7aa182a78a2c17f55ec980e515796dedc3
Aman Singh <114691636+amansinghas1734@users.noreply.github.com>
2026-09-21 00:17:01 +0530

    Time: 601 ms (10.63%), Space: 174.3 MB (10.9%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string expand(string s,int l,int r){
+        int i=0;
+        for(i;i<s.length();i++){
+            if(l-i<0||r+i>=s.length()||s[l-i]!=s[r+i]){
+                break;
+            }
+        }
+        cout<<"#"<<i;
+        return s.substr(l-i+1,2*i-l+r-1);
+    }
+    string longestPalindrome(string s) {
+        string l="";
+        for(int i=0;i<s.length();i++){
+            string op=expand(s,i,i);
+            cout<<op<<"-";
+            if(l.length()<op.length()){
+                l=op;
+            }
+            string ep=expand(s,i,i+1);
+            cout<<ep<<endl;
+            if(l.length()<ep.length()){
+                l=ep;
+            }
+        }
+        return l;
+    }
+};
